32ecdcbd8acc3e874580c34f94d51f7669cdb43a
Muhammad Zaki Ahmad <hawkmza2001@gmail.com>
2024-06-27 22:19:54 +0500

    Evaluation and Mlflow Integrated

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,11 @@
+import os
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig)
-import os
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                TrainingConfig,
+                                                EvaluationConfig)
+
 
 class ConfigurationManager:
     def __init__(
@@ -32,23 +36,24 @@ class ConfigurationManager:
     
 
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
-            config = self.config.prepare_base_model
-            
-            create_directories([config.root_dir])
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
 
-            prepare_base_model_config = PrepareBaseModelConfig(
-                root_dir=Path(config.root_dir),
-                base_model_path=Path(config.base_model_path),
-                updated_base_model_path=Path(config.updated_base_model_path),
-                params_image_size=self.params.IMAGE_SIZE,
-                params_learning_rate=self.params.LEARNING_RATE,
-                params_include_top=self.params.INCLUDE_TOP,
-                params_weights=self.params.WEIGHTS,
-                params_classes=self.params.CLASSES
-            )
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
 
-            return prepare_base_model_config
+        return prepare_base_model_config
     
+
 
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
@@ -71,3 +76,17 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/ftask297/EndToEndChestCancerClassificationUsingMlflowAndDVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
